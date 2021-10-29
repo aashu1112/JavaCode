f9b69c5ed3769c62dba44968d1047be2abb0a700
aashu1112 <saashu137@gmail.com>
2021-10-29 11:58:41 +0530

    my first commit

---
+++ src/test/java/javaCode/FirstCode.java
@@ -0,0 +1,9 @@
+package javaCode;
+
+public class FirstCode {
+
+	public static void main(String[] args) {
+		System.out.println("my first comit");
+	}
+
+}
---
+++ src/test/java/javaCode/SecondCode.java
@@ -0,0 +1,9 @@
+package javaCode;
+
+public class SecondCode {
+
+	public static void main(String[] args) {
+		System.out.println("my second comit");
+	}
+
+}
